85aaf4fb4d02d29e181a95088612872d492c9f4b
Zeeshan Ahmed <ziishaned@gmail.com>
2017-02-26 06:24:42 +0000

    Add page comments functionality

--- resources/views/page/partials/comment.blade.php
+++ resources/views/page/partials/comment.blade.php
@@ -20,7 +20,7 @@
         <div class="comments" style="height: 350px;">
             @if($page->comments->count() > 0)
                 @foreach($page->comments as $comment)
-                    <div class="comment">
+                    {{-- <div class="comment">
                         <div class="media">
                             <div class="pull-left">
                                 @if(!empty($comment->user->profile_image)) 
@@ -32,6 +32,48 @@
                             <div class="media-body">
                                 <h4 class="media-heading user-name"><a href="{{ route('users.show', [ $team->slug, $comment->user->slug ]) }}">{{ $comment->user->first_name . ' ' . $comment->user->last_name }}</a> <small class="comment-time" data-toggle="tooltip" data-placement="bottom" title="{{ $comment->created_at->timezone(Session::get('user_timezone'))->toFormattedDateString() . ' at ' . $comment->created_at->timezone(Session::get('user_timezone'))->format('h:i A')}}">{{ $comment->updated_at->diffForHumans() }}</small></h4>
                                 <p>{!! (new Emoji)->render($comment->content) !!}</p>
+                            </div>
+                        </div>
+                    </div> --}}
+                    <div class="comment" data-comment-id="{{ $comment->id }}">
+                        <div class="media">
+                            <div class="pull-left profile-image-con">
+                                @if(!empty($comment->user->profile_image)) 
+                                    <img class="media-object img-rounded profile-image" src="/img/{{ $comment->user->profile_image }}" alt="Image" width="44" height="44">
+                                @else
+                                    <img class="media-object img-rounded profile-image" src="/img/no-image.png" alt="Image" width="44" height="44">
+                                @endif
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading user-name"><a href="{{ route('users.show', [ $team->slug, $comment->user->slug ]) }}">{{ $comment->user->first_name . ' ' . $comment->user->last_name }}</a> <small class="comment-time" data-toggle="tooltip" data-placement="bottom" title="{{ $comment->created_at->timezone(Session::get('user_timezone'))->toFormattedDateString() . ' at ' . $comment->created_at->timezone(Session::get('user_timezone'))->format('h:i A')}}">{{ $comment->updated_at->diffForHumans() }}</small></h4>
+                                <div class="comment-body-con">
+                                    <div class="comment-body-inner">
+                                        <div class="comment-body">
+                                            <p class="comment-content" data-comment-content="{{ html_entity_decode(strip_tags($comment->content), null, 'utf-8') }}">{!! (new Emoji)->render($comment->content) !!}</p>
+                                        </div>
+                                        <div class="comment-actions">
+                                            <ul class="list-unstyled list-inline">
+                                                <li>
+                                                    <?php $userLikeComment = false; ?>
+                                                    @foreach($comment->likes as $like)
+                                                        @if($like->user_id === Auth::user()->id)
+                                                            <?php $userLikeComment = true; ?>
+                                                        @endif
+                                                    @endforeach
+                                                    <span class="label label-default" style="display: inline-block;" id="comment-like-counter">{{ $comment->likes->count() }}</span> <i class="fa fa-spinner fa-spin fa-lg fa-fw" id="spinner"></i> <a href="#" id="like-comment" data-comment-id="{{ $comment->id }}">{{ $userLikeComment ? 'Unlike' : 'Like' }}</a>
+                                                </li>
+                                                @if($comment->user_id === Auth::user()->id)
+                                                    <li>
+                                                        <a href="#" id="edit-comment">Edit</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#" id="delete-comment" data-comment-id="{{ $comment->id }}">Delete</a>
+                                                    </li>
+                                                @endif
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
