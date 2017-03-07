c1b1610b28b77262ad59cd274407e2c68556a19d
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-07 12:53:16 +0500

    Dashboard UI update

--- resources/views/partials/team-side-menu.blade.php
+++ resources/views/partials/team-side-menu.blade.php
@@ -4,31 +4,31 @@
 			<li class="nav-header">Quick Links</li>
 			<li class="item active">
 				<a href="dashboard.html">
-					<img src="/img/icons/basic_home.svg" width="24" height="24" class="icon">
-					<span class="item-name">Home</span>
+					<img src="/img/icons/basic_rss.svg" width="20" height="20" class="icon">
+					<span class="item-name">Activities</span>
 				</a>
 			</li>
 			<li class="item">
 				<a href="{{ route('teams.wikis', [$team->slug]) }}">
-					<img src="/img/icons/basic_book.svg" width="24" height="24" class="icon">
+					<img src="/img/icons/basic_book.svg" width="20" height="20" class="icon">
 					<span class="item-name">Wikis</span>
 				</a>
 			</li>
 			<li class="item">
 				<a href="{{ route('wikis.create', [ $team->slug ]) }}">
-					<img src="/img/icons/basic_book_pencil.svg" width="24" height="24" class="icon">
+					<img src="/img/icons/basic_book_pencil.svg" width="20" height="20" class="icon">
 					<span class="item-name">Create Wiki</span>
 				</a>
 			</li>
 			<li class="item">
 				<a href="{{ route('users.readlist', [$team->slug, Auth::user()->slug]) }}">
-					<img src="/img/icons/basic_todo_txt.svg" width="24" height="24" class="icon">
+					<img src="/img/icons/basic_todo_txt.svg" width="20" height="20" class="icon">
 					<span class="item-name">Read List</span>
 				</a>
 			</li>
 			<li class="item">
 				<a href="{{ route('teams.settings.members', [$team->slug,]) }}">
-					<img src="/img/icons/basic_mail.svg" width="24" height="24" class="icon">
+					<img src="/img/icons/basic_mail.svg" width="20" height="20" class="icon">
 					<span class="item-name">Invite User</span>
 				</a>
 			</li>
