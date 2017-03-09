0371cf8a0db2c52560f37808c205e306393f33cd
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-09 17:29:26 +0500

    Update app command

--- app/Console/Commands/AppCommand.php
+++ app/Console/Commands/AppCommand.php
@@ -7,44 +7,41 @@ use Illuminate\Console\Command;
 
 class AppCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app-setup';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Basic app setup';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
+    protected $commands = [
+        'config:cache',
+        'migrate:refresh',
+        [
+            'db:seed',
+            [
+                '--class' => 'PermissionTableSeeder',
+            ],
+        ],
+        [
+            'db:seed',
+            [
+                '--class' => 'IntegrationActionsTableSeeder'
+            ]
+        ]
+    ];
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        $this->call('config:cache');
-        $this->call('migrate:refresh');
-        $this->call('db:seed', [
-            '--class' => 'PermissionTableSeeder',
-        ]);
-        $this->call('db:seed', [
-            '--class' => 'IntegrationActionsTableSeeder',
-        ]);
+        foreach ($this->commands as $value) {
+            if (is_array($value)) {
+                $this->call($value[0], $value[1]);
+            } else {
+                $this->call($value);
+            }
+
+        }
     }
 }
