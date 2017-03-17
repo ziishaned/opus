c690cbd47699ef4680e7595a87928f7e63527480
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-17 17:32:57 +0500

    Page front ui tweaked

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -4,41 +4,45 @@
     @include('wiki.partials.menu')
     <div class="aside-content">
         <div class="row no-container">
-            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-                <div class="wiki-nav">
-                    <nav>
-                        <ul class="list-unstyled list-inline pull-left">
-                            <li>
-                                <a href="#"><img src="/img/icons/basic_todo_txt.svg" width="20" height="20" style="position: relative; top: -2px; margin-right: 3px;"> Add to Read list</a>
-                            </li>
-                        </ul>
-                        <ul class="list-unstyled list-inline pull-right">
-                            <li>
-                                <a href="{{ route('pages.edit', [$team->slug, $space->slug, $wiki->slug, $page->slug]) }}"><img src="/img/icons/software_pencil.svg" width="20" height="20" style="position: relative; top: -2px; margin-right: 3px;"> Edit</a>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="/img/icons/basic_gear.svg" width="20" height="20" style="position: relative; top: -2px; margin-right: 3px;"> Settings</a>
-                                <ul class="dropdown-menu dropdown-menu-right" style="margin-top: 8px;">
-                                    <li><a href="#"><i class="fa fa-info-circle fa-fw"></i> Page Overview</a></li>
-                                    <li><a href="#"><i class="fa fa-history fa-fw"></i> Page History</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#"><i class="fa fa-file-pdf-o fa-fw"></i> Export to PDF</a></li>
-                                    <li><a href="#"><i class="fa fa-file-word-o fa-fw"></i> Export to Word</a></li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a href="{{ route('pages.destroy', [$team->slug, $space->slug, $wiki->slug, $page->slug]) }}" data-method="delete" data-confirm="Are you sure?"><i class="fa fa-trash-o fa-fw"></i> Delete</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <div class="clearfix"></div>
-                    </nav>
-                </div>
-                <div class="markdown-body">
-                    {!! $page->description !!}
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div style="border: 1px solid #eee; border-radius: 3px; margin-bottom: 20px; box-shadow: 0 1px 1px rgba(0,0,0,.05);">
+                    <div class="wiki-nav">
+                        <nav>
+                            <ul class="list-unstyled list-inline pull-left">
+                                <li>
+                                    <a href="#"><img src="/img/icons/basic_todo_txt.svg" width="20" height="20" style="position: relative; top: -2px; margin-right: 3px;"> Add to Read list</a>
+                                </li>
+                            </ul>
+                            <ul class="list-unstyled list-inline pull-right">
+                                <li>
+                                    <a href="{{ route('pages.edit', [$team->slug, $space->slug, $wiki->slug, $page->slug]) }}"><img src="/img/icons/software_pencil.svg" width="20" height="20" style="position: relative; top: -2px; margin-right: 3px;"> Edit</a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="/img/icons/basic_gear.svg" width="20" height="20" style="position: relative; top: -2px; margin-right: 3px;"> Settings</a>
+                                    <ul class="dropdown-menu dropdown-menu-right" style="margin-top: 8px;">
+                                        <li><a href="#"><i class="fa fa-info-circle fa-fw"></i> Page Overview</a></li>
+                                        <li><a href="#"><i class="fa fa-history fa-fw"></i> Page History</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#"><i class="fa fa-file-pdf-o fa-fw"></i> Export to PDF</a></li>
+                                        <li><a href="#"><i class="fa fa-file-word-o fa-fw"></i> Export to Word</a></li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a href="{{ route('pages.destroy', [$team->slug, $space->slug, $wiki->slug, $page->slug]) }}" data-method="delete" data-confirm="Are you sure?"><i class="fa fa-trash-o fa-fw"></i> Delete</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </nav>
+                    </div>
+                    <div class="markdown-body" style="padding: 0px 25px;">
+                        {!! $page->description !!}
+                    </div>
                 </div>
             </div>
-            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        </div>
+        <div class="row no-container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 @include('page.partials.comment')
             </div>
         </div>
