afec31f3a39f48790c0deebda611f68c5becd904
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-17 15:56:19 +0500

    Create wiki page redesign

--- resources/views/wiki/create.blade.php
+++ resources/views/wiki/create.blade.php
@@ -9,13 +9,9 @@
 				</div>
 				<form action="{{ route('wikis.store', [$team->slug, ]) }}" method="POST" role="form" class="create-wiki-form">
 					<div class="row">
-						<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-							<h4>Basic information</h4>
-							<p class="text-muted">Beatae doloribus sapiente earum iusto hic labore porro facilis.</p>
-						</div>
-						<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 							<div class="row">
-								<div class="col-xs-6 col-sm-6 col-md-8 col-lg-8">
+								<div class="col-xs-6 col-sm-6 col-md-7 col-lg-7">
 									<div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
 										<label class="control-label" for="name">Name</label>
 										<input type="text" name="name" value="{{ old('name') }}" class="form-control" id="name" required>
@@ -24,7 +20,7 @@
 										@endif
 									</div>
 								</div>
-								<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="col-xs-6 col-sm-6 col-md-5 col-lg-5">
 									<div class="form-group {{ $errors->has('space') ? 'has-error' : '' }}">
 										<label for="space" class="control-label">Space</label>
 										<select name="space" id="space" class="form-control" required>
