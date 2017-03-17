8abfaa1894c884c388937dcb16510b0b8add98fb
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-17 18:12:56 +0500

    Replace svg with fontawesome ui tweaked

--- resources/views/team/dashboard.blade.php
+++ resources/views/team/dashboard.blade.php
@@ -6,7 +6,7 @@
 	<div class="aside-content">
 		<div class="row no-container">
 			<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-				<div class="page-header">Activities</div>
+				<div class="page-header"><i class="fa fa-history fa-fw fa-lg icon"></i> Activities</div>
 				@include('team.partials.activity')
 			</div>
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
