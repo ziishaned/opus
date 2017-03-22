a5a354bf5e8d13f1b61d9108016de9f49f671cad
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-22 17:54:32 +0500

    Update page pdf header

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -183,6 +183,38 @@ class WikiController extends Controller
 
     public function generatePdf(Team $team, Space $space, Wiki $wiki)
     {
+        $header = '
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Page header</title>
+                <style>
+                    * {
+                        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+                    }
+
+                    p {
+                        font-weight: 500;
+                        padding-bottom: 8px;
+                        border-bottom: 1px solid #eaecef;
+                        color: #c1c1c1;
+                    }
+
+                    h1 {
+                        padding-bottom: 8px;
+                    }
+                </style>
+            </head>
+            <body>
+                <p>
+                    '.$wiki->name.'
+                </p>
+                <h1></h1>
+            </body>
+            </html>
+        ';
+
         return \PDF::loadView('pdf.page', compact('wiki'))->inline($wiki->name . '.pdf');
     }
 }
