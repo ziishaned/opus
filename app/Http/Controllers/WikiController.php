2e3d77bc9a67a698e68c534796a1e194528ca88c
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-22 17:57:22 +0500

    Pass page header to pdf

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -215,6 +215,6 @@ class WikiController extends Controller
             </html>
         ';
 
-        return \PDF::loadView('pdf.page', compact('wiki'))->inline($wiki->name . '.pdf');
+        return \PDF::loadView('pdf.page', compact('wiki'))->setOption('header-html',$header)->inline($wiki->name . '.pdf');
     }
 }
