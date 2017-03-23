77b29a0f3eeedf4ded3e78626675b720b7a522e9
Zeeshan Ahmed <ziishaned@gmail.com>
2017-03-23 19:03:54 +0500

    Add watch wiki migration

---
+++ database/migrations/2017_03_23_140239_create_watch_wiki_tabel.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWatchWikiTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watch_wiki', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('wiki_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watch_wiki');
+    }
+}
