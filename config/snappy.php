a01104f6cf2676951c1e43d4ec9c620fbb793911
Simon White <Sekonda@users.noreply.github.com>
2017-04-20 20:12:31 +0100

    Change environment variable to WKHTMLTOPDF_PATH

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => env('SNAPPY_BINARY', 'C:/wkhtmltopdf/bin/wkhtmltopdf.exe'),
+        'binary'  => env('WKHTMLTOPDF_PATH', 'C:/wkhtmltopdf/bin/wkhtmltopdf.exe'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
